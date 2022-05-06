06a81fe442bad364ab1184ff7ccaec90f7abf3da
tanveer <mdtanviralamht731@gmail.com>
2022-05-06 10:34:38 +0600

    delivery button added

--- src/components/Inventory/InventoryDetail/InventoryDetail.js
+++ src/components/Inventory/InventoryDetail/InventoryDetail.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import delivery from "../../../img/truck8.png";
 
 const InventoryDetail = () => {
   const { id } = useParams();
@@ -9,9 +10,15 @@ const InventoryDetail = () => {
       .then((res) => res.json())
       .then((data) => setStone(data));
   }, [id]);
+  const handleDeliveryBtn = () => {
+    // fetch();
+  };
   const { name, img, price, quantity, supllier, description } = stone;
   return (
     <section className="min-h-screen max-w-[1100px] font-mono text-gray-600 mx-auto md:my-12 my-6">
+      {/* ------------------ */}
+      {/* details inventory  */}
+      {/* ------------------ */}
       <div className="detail flex-col md:flex-row flex justify-center items-center pt-8 ">
         <div className="detail-info md:w-[50%] w-full order-2 md:order-1">
           <h2 className="text-2xl md:text-6xl font-bold tracking-tighter md:my-6 my-4">
@@ -35,6 +42,15 @@ const InventoryDetail = () => {
             className="img-animation h-[200px] md:h-[400px] object-cover rounded-md shadow-lg"
           />
         </div>
+      </div>
+      <div className="deliverd my-12 p-4 flex justify-center ">
+        <button
+          onClick={handleDeliveryBtn}
+          className="flex md:text-2xl text-xl py-2 px-4 md:px-12 rounded-md border shadow-lg  items-center bg-white text-teal-500 hover:opacity-[0.7] duration-150 ease-in-out hover:bg-teal-50"
+        >
+          Delivered
+          <img src={delivery} alt="delivery" className="h-6 ml-2" />
+        </button>
       </div>
     </section>
   );
